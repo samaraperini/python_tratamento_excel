5da72d809bc6a2a170dc0092cb3ac3a3939ae9b5
Samara Perini <138031738+samaraperini@users.noreply.github.com>
2023-07-05 12:36:02 -0300

    Update main.py

--- tratamento_dados/main.py
+++ tratamento_dados/main.py
@@ -12,23 +12,23 @@ df_output['valor_juros'] = formatacao.preencher_valores_nulos(df_output['valor_j
 df_output['valor_face'] = df_output['valor_principal'] + df_output['valor_juros']
 
 for index, row in df_output.iterrows():
-    df_output.loc[index,'advogado_documento'] = formatacao.formatar_cpf_cnpj(row['advogado_documento'])
-    df_output.loc[index,'documento_credor'] = formatacao.formatar_cpf_cnpj(row['documento_credor'])
-    df_output.loc[index,'numero_processo'] = formatacao.formatar_precatorio_processo(row['numero_processo'])
-    df_output.loc[index,'numero_precatorio'] = formatacao.formatar_precatorio_processo(row['numero_precatorio'])
-    df_output.loc[index,'url_arquivo_oficio'] = row['numero_precatorio'] + '.pdf'
+    df_output.loc[index,'documento'] = formatacao.formatar_cpf_cnpj(row['documento'])
+    df_output.loc[index,'cliente'] = formatacao.formatar_cpf_cnpj(row['cliente'])
+    df_output.loc[index,'numero_processo'] = formatacao.formatar_processo(row['numero_processo'])
+    df_output.loc[index,'numero'] = formatacao.formatar_processo(row['numero'])
+    df_output.loc[index,'url_arquivo_oficio'] = row['numero'] + '.pdf'
     df_output.loc[index,'data_liquidacao'] = formatacao.formatar_data(row['data_liquidacao'])
     df_output.loc[index,'data_autuacao'] = formatacao.formatar_data(row['data_autuacao'])
-    df_output.loc[index,'data_transito_conhecimento'] = formatacao.formatar_data(row['data_transito_conhecimento'])
-    df_output.loc[index,'data_transito_execucao'] = formatacao.formatar_data(row['data_transito_execucao'])
+    df_output.loc[index,'data_conhecimento'] = formatacao.formatar_data(row['data_conhecimento'])
+    df_output.loc[index,'data_execucao'] = formatacao.formatar_data(row['data_execucao'])
     row['advogado_nome'] = str(row['advogado_nome']).replace('- ASSOCIADOS', 'ASSOCIADOS')
-    if row['advogado_nome'] is not None and str(row['advogado_nome']) != 'nan':
-        row['advogado_nome'] = row['advogado_nome'].split('-')
-        df_output.loc[index,'advogado_nome'] = row['advogado_nome'][0]
-        df_output.loc[index,'advogado_oab'] = row['advogado_nome'][1] 
+    if row['adv_nome'] is not None and str(row['adv_nome']) != 'nan':
+        row['adv_nome'] = row['adv_nome'].split('-')
+        df_output.loc[index,'adv_nome'] = row['adv_nome'][0]
+        df_output.loc[index,'adv_oab'] = row['adv_nome'][1] 
 
 for excluir_termos in formatacao.LISTA_TERMOS_EXCLUIR:
-    df_output['nome_credor'] = df_output['nome_credor'].replace(excluir_termos,'', regex=True)
+    df_output['cliente'] = df_output['cliente'].replace(excluir_termos,'', regex=True)
 
 df_output.to_excel(f'{OUTPUT_PATH}\\output_tratado.xlsx', engine='xlsxwriter', index=False)
 print('Output_tratado gerado com sucesso!')
